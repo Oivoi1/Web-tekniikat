b577b8a7dcc520f776e4d4cb7be4beb095384f5a
Veeti Oivo <MY_NAME@example.com>
2021-12-18 11:54:26 +0200

    Javascript-tehtävä alulleen ja muutoksia Bootstrap-tehtävään

--- JavaScriptTehtävä/Jotain.js
+++ JavaScriptTehtävä/Jotain.js
@@ -1,16 +1,20 @@
-// Let's create a HTTP request
-let xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET","https://yesno.wtf/api/",true);
-// Send request
-xmlhttp.send();
-
-// Let's create an event handler
-xmlhttp.onreadystatechange=function() {
-    if(xmlhttp.readyState === 4 && xmlhttp.status === 200)
-    {
-        let answerObject = JSON.parse(xmlhttp.response);
-        document.getElementById("answer").innerHTML= answerObject.answer;
-        document.getElementById("answer-image").setAttribute("src",answerObject.image);
-    }
+function getWeather() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+	 if (this.readyState == 4 && this.status == 200) {
+	    let weather = JSON.parse(this.response) 
+        // Access the result here
+	    let city = weather.location.name;
+        let tempature = weather.current.temp_c;
+        let tempatureFeels = weather.current.feelslike_c;
+        let weatherInfo = "In " + city + " there is " + tempature + " degree celcius and it feels like " + tempatureFeels + " degree celcius. "
+        document.getElementById("weatherInfo").innerHTML = weatherInfo
+	 }
+    };
+    xhttp.open("GET","http://api.weatherapi.com/v1/current.json?key=577191a56a1a41f2ab2152650211512&q=Oulu&aqi=no",true);
+    // Send request
+    xhttp.send();
 }
-  
+
+getWeather()
+
